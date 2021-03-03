65f20aa1ba22dcfaf6d55f3261efcb69e553b450
Aleff Alves <aleff.alves44@gmail.com>
2021-03-02 22:07:17 -0300

    adiciona novas informações

---
+++ js/main.js
@@ -0,0 +1,102 @@
+function clique(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //console.log(document.getElementById("agradecimento"));
+    //alert(clique)
+}
+
+function redirecionar(){
+    //window.open("https://github.com/aleffalves/");
+    window.location.href = "https://github.com/aleffalves";
+   // alert("Obrigado por clicar");
+}
+
+function trocar(){
+    document.getElementById("mouse()").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+
+/*function soma(n1,n2){
+    return n1 + n2;
+}
+*/
+
+
+
+
+/*
+var d = new Date();
+alert(d);
+alert(d.getDay)
+alert(d.getHours)
+*/
+
+
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count ++;
+
+}
+*/
+
+
+/*
+var idade = prompt("Qual sua idade?");
+if ( idade > 18) {
+    alert ("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+
+
+
+/*
+var frutas = [{nome:"maça", cor:"vermelho"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas.nome);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"maça", cor:"vermelho"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop()
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join("  -  "));
+
+
+
+/*
+
+//var nome = "Aleff Alves";
+//var idade = 24;
+//var idade2 = 22;
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos!");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"))
+*/
